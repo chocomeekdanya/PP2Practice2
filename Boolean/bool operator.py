9f35562c6ab63d909bf177d1de35dbefb9379b51
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 10:44:50 +0600

    Create bool operator.py

---
+++ Boolean/bool operator.py
@@ -0,0 +1,9 @@
+bool("abc") #True
+bool(123) #True
+bool(["apple", "cherry", "banana"]) #True
+bool(0)
+bool(False)
+bool("")
+bool(())
+bool([])
+bool({}) #Empty values are automatically evaluated as False
