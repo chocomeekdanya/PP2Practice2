da80afb22d4ef49d8495566372c98c8d682628b7
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:32:17 +0600

    Create logical if.py

---
+++ If-else/logical if.py
@@ -0,0 +1,16 @@
+a = 221
+b = 15
+c = 5000
+if a > b and c > a:
+  print("Both conditions are True") #returns True if both conditions are satisfied
+#----------
+a = 221
+b = 15
+c = 5000
+if a > b or a > c:
+  print("At least one of the conditions is True") #returns True if either of condtions are true
+#------------
+a = 1
+b = 500
+if not a > b:
+  print("a is NOT greater than b") #tests if a is NOT greater than B
