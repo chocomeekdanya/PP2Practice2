975f1de4414066ee1f29701345904cd6a0a9d4b1
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 10:50:07 +0600

    Create If statement.py

---
+++ If-else/If statement.py
@@ -0,0 +1,8 @@
+number = 15
+if number > 0:
+  print("The number is positive") #The if statement evaluates a condition (an expression that results in True or False). 
+ #If the condition is true, the code block inside the if statement is executed. If the condition is false, the code block is skipped.
+  a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
