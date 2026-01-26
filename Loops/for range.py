917257d34cddd181f770ae78c6cd2a71a0ccfd86
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:27:56 +0600

    Create for range.py

---
+++ Loops/for range.py
@@ -0,0 +1,14 @@
+#-------- Example 1
+for i in range(5):
+    print(i)
+
+#------- Example 2
+for i in range(2, 7):
+    print(i)
+
+#------- Example 3
+
+for i in range(1, 10, 2): #2 is difference between each element, so output gonna be 1, 3, 5, 7, 9
+    print(i)
+
+
