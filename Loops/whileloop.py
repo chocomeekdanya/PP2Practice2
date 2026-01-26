886b18d565646f2444c5bf354b4c16f0836aa8ef
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:16:57 +0600

    Create whileloop.py

---
+++ Loops/whileloop.py
@@ -0,0 +1,11 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1 #incrementation is important or code will go on forever
+# ------------   while else example
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6") #break will hold this block of code against executing
