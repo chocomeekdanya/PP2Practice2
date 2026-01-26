c8d990425cc7b7718a7a8cb46aeb2ee0305a214d
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:23:22 +0600

    Create for break.py

---
+++ Loops/for break.py
@@ -0,0 +1,11 @@
+cars = ["Toyota", "Lexus", "Bentley"]
+for x in cars:
+  print(x)
+  if x == "Lexus":
+    break
+    #---------------
+    cars = ["Toyota", "Lexus", "Bentley"]
+for x in cars:
+  if x == "Lexus":
+    break
+  print(x)
