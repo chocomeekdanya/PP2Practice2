46d1b3e4e559fdf69b7f9e2cc470d40907cdac92
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:11:07 +0600

    Create if-else.py

---
+++ If-else/if-else.py
@@ -0,0 +1,15 @@
+a = 121
+b = 74
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b") #f no condition passed, compiler considers alternative "else" case and outputs its print
+#----------
+number = 61
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
