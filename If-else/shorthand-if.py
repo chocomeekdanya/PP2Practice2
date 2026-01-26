2ba0095d8bd59ee5092edb67ced77580e6e46654
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:15:00 +0600

    Create shorthand-if.py

---
+++ If-else/shorthand-if.py
@@ -0,0 +1,9 @@
+#there is way to write If-condition in one line - Shorthand if method
+a = 25
+b = 22
+if a > b: print("a is greater than b")
+  #-----------    
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
