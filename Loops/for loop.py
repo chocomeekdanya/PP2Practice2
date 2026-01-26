a94a9f569ef468201a0cb7fcb69b84fadd3ec350
pushkin daniil <danilkeklol9@gmail.com>
2026-01-26 11:21:01 +0600

    Create for loop.py

---
+++ Loops/for loop.py
@@ -0,0 +1,6 @@
+cars = ["Lexus", "Toyota", "Bentley", "Chevrolet"]
+for x in cars:
+  print(x) #The for loop does not require an indexing variable to set beforehand.
+#------------ loop through string
+for k in "University":
+  print(k)
