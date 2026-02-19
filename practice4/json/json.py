7adeef991126f62ab124ef88d3f77fc0c5f70566
pushkin daniil <danilkeklol9@gmail.com>
2026-02-20 00:42:18 +0600

    Update json.py

--- practice4/json/json.py
+++ practice4/json/json.py
@@ -1,6 +1,5 @@
 import json
 
-# open JSON file
 with open("sample-data.json", "r") as file:
     data = json.load(file)
 
@@ -9,7 +8,7 @@ print("=" * 80)
 print(f"{'DN':50} {'Description':20} {'Speed':10} {'MTU':5}")
 print("-" * 80)
 
-# navigate through JSON structure
+
 interfaces = data["imdata"]
 
 for item in interfaces:
