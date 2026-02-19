e5a4ad385ec873b741979521cf18f48fae56aa1a
pushkin daniil <danilkeklol9@gmail.com>
2026-02-20 00:38:29 +0600

    Create 2.py

---
+++ practice4/date/2.py
@@ -0,0 +1,10 @@
+height = 5
+base1 = 5
+base2 = 6
+
+area = 0.5 * (base1 + base2) * height
+
+print("Height:", height)
+print("Base, first value:", base1)
+print("Base, second value:", base2)
+print("Expected Output:", area)
