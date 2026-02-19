02559ab7123c44ae2d68f42d2a3a69057034a864
pushkin daniil <danilkeklol9@gmail.com>
2026-02-20 00:42:04 +0600

    Update json.py

--- practice4/json/json.py
+++ practice4/json/json.py
@@ -1,1 +1,17 @@
+import json
+
+# open JSON file
+with open("sample-data.json", "r") as file:
+    data = json.load(file)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':50} {'Description':20} {'Speed':10} {'MTU':5}")
+print("-" * 80)
+
+# navigate through JSON structure
+interfaces = data["imdata"]
+
+for item in interfaces:
+    att
 
