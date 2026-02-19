826af3f98ab818ea42d7bf7e3ff128644745dcf4
pushkin daniil <danilkeklol9@gmail.com>
2026-02-20 00:36:06 +0600

    Update 1.py

--- practice4/math/1.py
+++ practice4/math/1.py
@@ -1,1 +1,8 @@
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+new_date = current_date - timedelta(days=5)
+
+print("Current date:", current_date)
+print("Date minus 5 days:", new_date)
 
