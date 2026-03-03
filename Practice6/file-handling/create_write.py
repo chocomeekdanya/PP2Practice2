8a7b278f48d2eb2fcb697278adc918a60fb755e2
pushkin daniil <danilkeklol9@gmail.com>
2026-03-03 12:17:57 +0600

    Create create_write.py

---
+++ Practice6/file-handling/create_write.py
@@ -0,0 +1,8 @@
+
+
+with open("sample.txt", "w") as file:
+    file.write("Hello World\n")
+    file.write("This is a sample file.\n")
+    file.write("Python file handling practice.\n")
+
+print("File created and data written successfully.")
